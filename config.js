fc7469647ac8d1e2a55fb885dbc1ebd7d8c5365b
PrimeBot69 <156514782+PrimeBot69@users.noreply.github.com>
2024-01-14 11:56:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,14 +11,14 @@ global.owner = [
   ['233208179431', 'Abraham Dwamena', true],
   ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true], 
-  
+  ['916001031685', '亗一『Ｐｒｉｍｅ』・', true], 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+global.pairingNumber = "916001031685"//put your bot number here
 
-global.mods = ['923184070915', '94789481495', '923184474176','233533763772','233208179431'] 
-global.prems = ['923184070915', '233208179431', '94789481495', '918360234087']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087']
+global.mods = ['923184070915', '94789481495', '923184474176','233533763772','233208179431', '916001031685'] 
+global.prems = ['923184070915', '233208179431', '94789481495', '918360234087', '916001031685']
+global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087', '916001031685']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -53,10 +53,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = '『Ｐｒｉｍｅ』-V2'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = '亗一『Ｐｒｉｍｅ』・ TEAM' 
+global.author = '@『Ｐｒｉｍｅ』-V2' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
@@ -67,7 +67,7 @@ global.fglog = 'XLICON.jpg'
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
 
-global.wait = '*🕣 _XLICON IS LODIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 _亗一『Ｐｒｉｍｅ』・ IS LODIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✔️'
